b08c250cbcc7e0e018ee8e5f1cc78d0729793488
‘stanams’ <stan.amsellem@gmail.com;GIT_COMMITTER_NAME=stanams;GIT_COMMITTER_EMAIL=stan.amsellem@gmail.com>
2016-02-15 09:53:08 -0800

    initial commit webpack setup

---
+++ main.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  ReactDOM.render(<MyComponent />, document.getElementById('main'));
+});
